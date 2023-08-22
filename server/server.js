a579ec96bb15f451615b6f6265eba148013023c2
MartinMeng1 <126767342+MartinMeng1@users.noreply.github.com>
2023-08-21 18:26:00 -0700

    initial commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('../client/dist'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+
+// 1.Package.json config
+// 2.Webpackconfig.js
+// 3.indexDB routing 
+// 4.Installtion
